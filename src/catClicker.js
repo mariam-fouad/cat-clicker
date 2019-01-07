c5a9394656269710ef8225df2f89705f97bd9118
Mariam <mariam.fouad94@gmail.com>
2019-01-08 00:10:25 +0300

    change the cats value when the cat name is clicked

--- src/catClicker.js
+++ src/catClicker.js
@@ -1,12 +1,20 @@
 //cats names
 const cats=["Elle","Lou","Lele","Ed","Row","Koje"];
 
+const showCat = index=>{
+    document.querySelector('.cat-name').innerHTML=cats[index];
+    document.querySelector('.cat-counter').innerHTML=0;
+    document.querySelector('.cat-area').src=`assets/cat${index+1}.jpg`
+}
 //loop over all the cats
-cats.forEach((cat)=>{
+cats.forEach((cat , index)=>{
     const listElement = document.createElement("li");
     const name = document.createTextNode(cat);
 
     listElement.appendChild(name);
 
+    listElement.addEventListener('click',()=>{
+        showCat(index);
+    });
     document.querySelector('ul').appendChild(listElement);
 });
