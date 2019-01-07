32fdf2484f1e3d35adf9004f87efad90e4880627
Mariam <mariam.fouad94@gmail.com>
2019-01-07 23:59:24 +0300

    display the list of cats

--- src/catClicker.js
+++ src/catClicker.js
@@ -1,22 +1,12 @@
-let counter1 = 0;
-let counter2 = 0;
-const catName1 = "Lou";
+//cats names
+const cats=["Elle","Lou","Lele","Ed","Row","Koje"];
 
-const catName2 ="elle";
+//loop over all the cats
+cats.forEach((cat)=>{
+    const listElement = document.createElement("li");
+    const name = document.createTextNode(cat);
 
-const IncrementCounter = (event)=>{
-    if(event.target.classList.contains('cat1-img')){
-        counter1++;
-        document.querySelector('.cat1-counter').innerHTML=counter1;
-    }
-    else{
-        counter2++;
-        document.querySelector('.cat2-counter').innerHTML=counter2;
-    }
-    
-} 
-document.querySelector('.cat1-name').innerHTML=catName1;
-document.querySelector('.cat2-name').innerHTML=catName2;
+    listElement.appendChild(name);
 
-document.querySelector('.cat1-img').addEventListener('click',IncrementCounter);
-document.querySelector('.cat2-img').addEventListener('click',IncrementCounter);
+    document.querySelector('ul').appendChild(listElement);
+});
