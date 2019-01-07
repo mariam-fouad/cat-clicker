c5acf3f2bc8230deacc429b9da8469c79f9caa7a
Mariam <mariam.fouad94@gmail.com>
2019-01-07 21:11:08 +0300

    make the catClicker with two cats

--- src/catClicker.js
+++ src/catClicker.js
@@ -1,7 +1,22 @@
-let counter = 0;
+let counter1 = 0;
+let counter2 = 0;
+const catName1 = "Lou";
 
-const IncrementCounter = ()=>{
-    counter++;
-    document.querySelector('.counter').innerHTML=counter;
+const catName2 ="elle";
+
+const IncrementCounter = (event)=>{
+    if(event.target.classList.contains('cat1-img')){
+        counter1++;
+        document.querySelector('.cat1-counter').innerHTML=counter1;
+    }
+    else{
+        counter2++;
+        document.querySelector('.cat2-counter').innerHTML=counter2;
+    }
+    
 } 
-document.querySelector('.cat').addEventListener('click',IncrementCounter);
+document.querySelector('.cat1-name').innerHTML=catName1;
+document.querySelector('.cat2-name').innerHTML=catName2;
+
+document.querySelector('.cat1-img').addEventListener('click',IncrementCounter);
+document.querySelector('.cat2-img').addEventListener('click',IncrementCounter);
